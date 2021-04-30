c1f1c9ae51a95e928076d78826a5fce95f20183d
André Pascal Beyer <mail@beyeran.site>
2021-04-30 08:24:31 +0200

    [keymap] added missing asterisk.

--- keyboards/preonic/keymaps/beyeran/keymap.c
+++ keyboards/preonic/keymaps/beyeran/keymap.c
@@ -89,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT_preonic_grid(
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,      KC_F10,  KC_F11,  KC_F12,
     XXXXXXX, DE_ACUT, DE_UNDS, DE_LBRC, DE_RBRC, DE_LABK, DE_EXLM, DE_CIRC, S(DE_CIRC), DE_EQL,  DE_AMPR, XXXXXXX,
-    XXXXXXX, DE_BSLS, DE_SLSH, DE_LCBR, DE_RCBR, DE_SLSH, DE_QUES, DE_LPRN, DE_RPRN,    DE_MINS, DE_COLN, DE_AT,
+    XXXXXXX, DE_BSLS, DE_SLSH, DE_LCBR, DE_RCBR, DE_ASTR, DE_QUES, DE_LPRN, DE_RPRN,    DE_MINS, DE_COLN, DE_AT,
     XXXXXXX, DE_HASH, DE_DLR,  DE_PIPE, DE_TILD, DE_GRV,  DE_PLUS, DE_PERC, DE_DQUO,    DE_QUOT, DE_SCLN, XXXXXXX,
     _______, _______, _______, _______, _______, _______, _______, _______, XXXXXXX,    _______, _______, _______
 ),
